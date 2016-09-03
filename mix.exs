e92cbbf2c78108694a829cb27f5bc344ffd76980
Brian van Burken <brianvanburken@outlook.com>
2016-09-03 09:35:32 +0200

    Set version to beta.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Endo.Mixfile do
 
   def project do
     [app: :endo,
-     version: "0.1.0",
+     version: "0.0.1-beta",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
